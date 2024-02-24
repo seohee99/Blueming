c8036293849e8e5f1766099885cdf72981c8f03a
ekgus9701@gmail.com <52192706+ekgus9701@users.noreply.github.com>
2024-02-24 17:09:33 +0900

    Feat: 자유 게시판 상세 페이지 틀 구현

--- frontend/src/routes/board/board/detail/BoardDetailPage.jsx
+++ frontend/src/routes/board/board/detail/BoardDetailPage.jsx
@@ -14,32 +14,59 @@ export default function BoardDetailPage() {
 
   const commentData = [
     {
+      writer: "댓1",
+      date: "2024.02.19",
       commentContent: "나도 어려워",
       isAnonymous: 1,
+    },
+    {
+      writer: "댓2",
+      date: "2024.02.19",
+      commentContent: "ㅠㅠ 나도 모름",
+      isAnonymous: 0,
+    },
+    {
+      writer: "댓3",
+      date: "2024.02.19",
+      commentContent: "화이팅...",
+      isAnonymous: 0,
     },
   ];
   return (
     <Container className="board-detail-page">
       <div className="board-detail-all">
-        <div className="board-detail-title-tag">
-          <div className="board-detail-title">{boardDetailData.title}</div>
+        <div className="board-detail-tag-container">
           {boardDetailData.tag.map((boardDetailTag) => (
             <div className="board-detail-tag">{boardDetailTag}</div>
           ))}
-
-          <div className="board-writer-date">
-            {boardDetailData.isAnonymous ? Anonymous : boardDetailData.writer} /
-            {boardDetailData.date}
-          </div>
-          <div className="board-content">{boardDetailData.content}</div>
         </div>
-        <Button className="edit-board-btn" onClick={() => {}}>
-          수정
-        </Button>
+        <div className="board-detail-title">
+          <h1>{boardDetailData.title}</h1>
+        </div>
+
+        <div className="board-writer-date-btns">
+          <div className="board-writer-date">
+            <strong>
+              {boardDetailData.isAnonymous ? Anonymous : boardDetailData.writer}
+            </strong>{" "}
+            / {boardDetailData.date}
+          </div>
+          <div className="board-detail-btns">
+            <button className="board-btn-edit" onClick={() => {}}>
+              수정
+            </button>
+            <button className="board-btn-del" onClick={() => {}}>
+              삭제
+            </button>
+          </div>
+        </div>
+        <div className="board-content">{boardDetailData.content}</div>
       </div>
       <hr className="line"></hr>
       <div className="board-comment-all">
-        <div className="board=comment-num">{commentData.length}개의 댓글</div>
+        <div className="board-comment-num">
+          <h5>{commentData.length}개의 댓글</h5>
+        </div>
         <div className="board-comment-write">
           <Form.Control
             className="comment-write-form"
@@ -49,15 +76,22 @@ export default function BoardDetailPage() {
             onKeyUp={(e) => onKeyUp(e)}
           />
           <Button className="comment-write-btn" onClick={() => {}}>
-            댓글 작성
+            작성
           </Button>
         </div>
 
-        <div className="board-comment-writer-date">
-          <div className="board-comment-writer"></div>
-          <div className="board-comment-date"></div>
+        <div className="board-comment-view">
+          {commentData.map((data) => (
+            <div className="board-comment-each">
+              <div className="board-comment-writer">
+                <strong>{data.isAnonymous ? "Anonymous" : data.writer}</strong>
+              </div>
+              <div className="board-comment-date">{data.date}</div>
+              <div className="board-comment-content">{data.commentContent}</div>
+              <hr className="line" />
+            </div>
+          ))}
         </div>
-        <div className="board-comment"></div>
       </div>
     </Container>
   );
