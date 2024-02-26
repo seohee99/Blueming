e6bf255b97d0e3347f38fdc223be39ebd2b49af8
Sieun Kim <se0983@kookmin.ac.kr>
2024-02-26 11:19:13 +0900

    Fix: 화면 렌더링 오류 수정

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -1,21 +1,20 @@
 // MainLayout.js
-import React from 'react'
-import Header from './Header'
-import Sidebar from './Sidebar'
-import { Container } from 'react-bootstrap'
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer'
+import React from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 
 export default function MainLayout() {
   return (
     <>
       <Sidebar />
       <Header />
-      <Container style={{ marginLeft: '200px' }}>
-
+      <Container style={{ marginLeft: "200px", marginTop: "200px" }}>
         <Outlet />
       </Container>
       <Footer />
     </>
-  )
+  );
 }
