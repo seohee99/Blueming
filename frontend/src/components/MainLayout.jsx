be97cb6d6f5203c98d191da14ec0f3e5ab997940
ekgus9701@gmail.com <52192706+ekgus9701@users.noreply.github.com>
2024-02-26 16:28:20 +0900

    Style: 메인 레이아웃 변경

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -1,23 +1,30 @@
 // MainLayout.js
-import React from 'react'
-import Header from './Header'
-import Sidebar from './Sidebar'
-import { Container } from 'react-bootstrap'
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer'
+import React from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 
 export default function MainLayout() {
   return (
     <>
-      <Sidebar />
       <Header />
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <Container style={{ marginTop: '100px', backgroundColor: 'ivory' }}>
-
-          <Outlet />
-        </Container>
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <div style={{ marginLeft: "200px", flex: 1 }}>
+          <Container
+            style={{
+              marginTop: "100px",
+              backgroundColor: "ivory",
+              padding: "20px",
+            }}
+          >
+            <Outlet />
+          </Container>
+        </div>
       </div>
       <Footer />
     </>
-  )
+  );
 }
