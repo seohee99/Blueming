c1b44a739cf3ad2d3313c47cce5474ef8f858d89
ekgus9701@gmail.com <52192706+ekgus9701@users.noreply.github.com>
2024-02-27 16:51:23 +0900

    Style: css 수정

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -15,7 +15,7 @@ export default function MainLayout() {
         <div style={{ marginLeft: "200px", flex: 1 }}>
           <Container
             style={{
-              marginTop: "100px",
+              marginTop: "60px",
               backgroundColor: "white",
               padding: "20px",
             }}
