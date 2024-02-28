534beb590d07d9e24333fa9db57ee5b43b72bde8
seohee99 <imshee99@gmail.com>
2024-02-29 05:24:18 +0900

    Feat: 알림 스타일 변경

--- frontend/src/routes/question/AlarmList.jsx
+++ frontend/src/routes/question/AlarmList.jsx
@@ -32,19 +32,49 @@ export default function AlarmList({ handleShowAlarmList }) {
 
     const filteredAlarms = showAll ? alarms : alarms.filter(alarm => !alarm.confirmed);
 
+    const getAlertColor = (title) => {
+        switch (title) {
+            case "코드를 다시 보여주세요":
+                return '#F0FFFF';  // 빨간색
+            case "너무 빨라요":
+                return '#FFE4E1';  // 초록색
+            case "다시 한번 설명해 주세요":
+                return '#FFF8DC';  // 파란색
+            default:
+                return '#edeceb';  // 기본색
+        }
+    };
+
+    const calculateTime = (pastTime) => {
+        const timeDifference = new Date() - new Date(pastTime);
+        const seconds = Math.floor(timeDifference / 1000);
+        const minutes = Math.floor(seconds / 60);
+        const hours = Math.floor(minutes / 60);
+        const days = Math.floor(hours / 24);
+
+        if (days > 0) return `${days}일 전`;
+        if (hours > 0) return `${hours}시간 전`;
+        if (minutes > 0) return `${minutes}분 전`;
+        return `${seconds}초 전`;
+    };
+
     return (
-        <Modal show={true} onHide={handleShowAlarmList} style={{ width: '500px' }}>
+        <Modal show={true} onHide={handleShowAlarmList} >
             <Modal.Header closeButton>
-                <Modal.Title>알림 보기</Modal.Title>
+                <Modal.Title centered>알림 보기</Modal.Title>
             </Modal.Header>
-            <Modal.Body style={{ maxHeight: '300px', overflow: 'auto' }}>
-                <Button onClick={() => setShowAll(false)}>확인 안한 알림만 보기</Button>
-                <Button onClick={() => setShowAll(true)}>전체 보기</Button>
+            <Modal.Body style={{ maxHeight: '400px', overflow: 'auto' }}>
+                <div style={{ display: 'flex', justifyContent: 'flex-end', gap: 10, marginBottom: '5%' }}>
+                    <Button onClick={() => setShowAll(false)}>✨NEW</Button>
+                    <Button onClick={() => setShowAll(true)}>🔍View All</Button>
+                </div>
                 {filteredAlarms.map(alarm =>
-                    <Alert key={alarm._id} style={{ backgroundColor: '#ebf1ff' }} onClose={() => handleConfirm(alarm._id)} dismissible>
-                        <p>제목: {alarm.alarmTitle}</p>
-                        <p>작성일: {new Date(alarm.alarmCreatedAt).toLocaleString()}</p>
-                        <p>학생: {alarm.userName}</p>
+                    <Alert key={alarm._id} style={{ backgroundColor: getAlertColor(alarm.alarmTitle), border: 'none' }} onClose={() => handleConfirm(alarm._id)} dismissible>
+                        <span>  🏷️ {alarm.alarmTitle}</span>
+                        <div style={{ display: 'flex', justifyContent: 'flex-end', gap: 10 }}>
+                            <span style={{ backgroundColor: '#F5F5F5', borderRadius: '10px', padding: '5px' }}> {alarm.userName} </span>
+                            <span style={{ padding: '5px' }}>  {calculateTime(alarm.alarmCreatedAt)}</span>
+                        </div>
                     </Alert>
                 )}
             </Modal.Body>
